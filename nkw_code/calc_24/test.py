fa2c5e60a530f6703cbe331907f6beaaa9314592
ApollonKangGitHub <mailbox_krj@163.com>
2020-06-06 14:42:28 +0800

    扑克计算24优化与迭代器标准库学习

--- nkw_code/calc_24/test.py
+++ nkw_code/calc_24/test.py
@@ -13,20 +13,51 @@
 import unittest
 from calc_24 import valid
 import calc_24
+#全排列函数
+from itertools import permutations
+#全组合函数
+from itertools import combinations
 
 class test24(unittest.TestCase):
-    def test_find_24(self):
-        with open("24_success.txt", 'w+') as obj_ok:
-            with open("24_fail.txt", 'w+') as obj_err:
+	#所有值可复用（num^n）
+    def test_all_24(self):
+        with open("24_success_all.txt", 'w+') as obj_ok:
+            with open("24_fail_all.txt", 'w+') as obj_err:
                 for v1 in valid.keys():
                     for v2 in valid.keys():
                         for v3 in valid.keys():
                             for v4 in valid.keys():
-                                ret = calc_24.main(str(v1) + ' ' + str(v2) + ' ' + str(v3) + ' ' + str(v4))
+                                ret = calc_24.main(v1 + ' ' + v2 + ' ' + v3 + ' ' + v4)
                                 ret += '\n'
                                 if '=' in ret:
                                     obj_ok.write(ret);
                                 else:
                                     obj_err.write(ret);
+                                    
+	#所有值只能用一次（全排列）
+    def test_permutations_24(self):
+        with open("24_success_permutations.txt", 'w+') as obj_ok:
+            with open("24_fail_permutations.txt", 'w+') as obj_err:
+                for v1,v2,v3,v4 in permutations(valid.keys(), 4):
+                    #print(v1,v2,v3,v4)
+                    ret = calc_24.main(v1 + ' ' + v2 + ' ' + v3 + ' ' + v4)
+                    ret += '\n'
+                    if '=' in ret:
+                        obj_ok.write(ret);
+                    else:
+                        obj_err.write(ret);
+                        
+	#所有值只能用一次（全组合）
+    def test_combinations_24(self):
+        with open("24_success_combinations.txt", 'w+') as obj_ok:
+            with open("24_fail_combinations.txt", 'w+') as obj_err:
+                for v1,v2,v3,v4 in combinations(valid.keys(), 4):
+                    #print(v1,v2,v3,v4)
+                    ret = calc_24.main(v1 + ' ' + v2 + ' ' + v3 + ' ' + v4)
+                    ret += '\n'
+                    if '=' in ret:
+                        obj_ok.write(ret);
+                    else:
+                        obj_err.write(ret);                       
 #测试父类在程序退出后开始测试               
 unittest.main()
